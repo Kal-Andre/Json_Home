bd89f6c2bf258c8e277bf5a4696da5c818399853
Kal-Andre <andrewlou333@gmail.com>
2024-05-08 00:24:46 +0300

    This one ensures that the JSON fields are present in the JSON.

---
+++ beginner/Assignment5.py
@@ -0,0 +1,6 @@
+'''JSON DATA VALIDATOR
+
+This program reads JSON data representing student records(name, agee, grade, etc)
+and validates each record ensuring that all required fields are present and have valid values.'''
+
+import json
